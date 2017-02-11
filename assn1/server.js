b764414bf01f467530325c815e0604d38f970eec
Khalil <khalil.va@gmail.com>
2017-02-11 13:14:35 -0500

    updated server

---
+++ assn1/server.js
@@ -0,0 +1,56 @@
+
+
+/** Load Modules **/
+
+var http = require('http');
+var fs = require('fs');
+var mime = require('mime-types');
+
+/** Create server **/
+
+const PORT = 2406;
+var server = http.createServer(handleRequest);
+server.listen(PORT);
+console.log('SServer started [' + PORT + ']');
+
+/** Request handler **/
+
+function handleRequest(req, res) {
+	console.log("Request: "+ req.url);
+	var filename = "."  + req.url; 	
+	var code=500;
+	var data = "";
+
+	if (req.url == "/allheroes"){
+		var heroes = fs.readdirSync('./heroes');
+		res.writeHead(200, {'content-Type': 'application/json'});
+		res.write(JSON.stringify(heroes));
+		res.end(data);
+		return;
+	}
+
+	if (req.url.startsWith('/hero')){
+		var heroname = req.url.split("=")[1];
+		if(fs.existsSync("./heroes/" + heroname)){
+			data = fs.readFileSync("./heroes/" + heroname);
+			code = 200;
+		}else{
+			console.log("Hero file not found " + heroname);
+			code = 404;
+		}
+	} else if(fs.existsSync(filename)){		
+		var stats = fs.statSync(filename);
+		if(stats.isDirectory()){
+			filename += "/index.html";
+		}
+		console.log("Getting file: "+filename);
+		data = fs.readFileSync(filename);
+		code = 200;
+	}else{
+		console.log("File not found " + heroname);
+		code = 404;
+	}
+	
+	res.writeHead(code, {'content-type': mime.lookup(filename)|| 'text/html'});
+	res.end(data);
+};
