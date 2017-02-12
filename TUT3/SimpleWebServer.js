01bb3f242caba44e94a8c15e4bffbde0f08d520f
Khalil <khalil.va@gmail.com>
2017-02-12 01:50:38 -0500

    final edits

--- TUT3/SimpleWebServer.js
+++ TUT3/SimpleWebServer.js
@@ -4,7 +4,7 @@
 var http = require('http');
 var fs = require('fs');
 var mime = require('mime-types');
-const ROOT = "./public";
+const ROOT = "./public_html";
 
 // create http server
 var server = http.createServer(handleRequest);
@@ -13,36 +13,37 @@ console.log('Server listening on port 2406');
 
 // handler for incoming requests
 function handleRequest(req, res) {
-	//process the request
 	console.log("Request for: "+req.url);
-	var filename = ROOT+req.url;
-
-	var code;
 	var data = "";
 
-	var stats = fs.statSync(filename);
-	if(stats.isDirectory()){
-		data = getFileContents("index.html");
-	}else{
-		try{
-			data = getFileContents(filename);
-		} catch (err){
+	if (req.url.endsWith('.html')){
+		console.log("HTML requested");
+		if(fs.existsSync(ROOT + req.url)){
+			data = getFileContents(ROOT + req.url);
+		}else{
 			data = getFileContents(ROOT+"/404.html");
+		}
+	}else {
+
+		var stats = fs.statSync("."+req.url);
+		if(stats.isDirectory()){
+			data = getFileContents(ROOT+"/index.html");
+		}else{
+			try{
+				data = getFileContents("."+req.url);
+			} catch (err){
+				data = getFileContents(ROOT+"/404.html");
+			}
 		}
 	}
 	code = 200;
-	// content header
-	res.writeHead(code, {'content-type': mime.lookup(filename)||'text/html'});
-	// write message and signal communication is complete
+	res.writeHead(code, {'content-type': mime.lookup(req.url)||'text/html'});
 	res.end(data);
 };
 
-//read a file and returns its contents
+
 function getFileContents(filename){
-	
 	var contents;
-	
-	//handle good requests
 	console.log("Getting file");
 	contents = fs.readFileSync(filename);
 	console.log("typeof: "+typeof(contents));
